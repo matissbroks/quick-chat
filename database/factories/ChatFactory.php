930f7e06adf6f786598654410cac76c1be8d0ec8
Matīss Broks <Matiss.Broks@rtu.lv>
2023-09-30 23:02:29 +0300

    Chat and ChatUser migrations, factories and database seeder updated/created

---
+++ database/factories/ChatFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends Factory<\App\Models\Chat>
+ */
+class ChatFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->domainName(),
+            'unique_name' => Str::random(10),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Chat;
+use App\Models\ChatUser;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+         Chat::factory(3)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         for ($i = 1; $i <= 10; $i++) {
+             $id = rand(1, 3);
+             $radn_usr_count = rand(1, 5);
+             ChatUser::factory($radn_usr_count)->create([
+                 'chat_id' => $id
+             ]);
+         }
     }
 }
